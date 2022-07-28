f9d6f9f29b5a8dc7db318ba27dd137c74b99c64e
Nascimento <nascimento231216@gmail.com>
2022-07-28 12:57:39 -0300

    temp

--- lambda_alocacao_usina_cliente/src/main/java/br/com/eoxygen/service/UsinaService.java
+++ lambda_alocacao_usina_cliente/src/main/java/br/com/eoxygen/service/UsinaService.java
@@ -1,55 +1,28 @@
 package br.com.eoxygen.service;
-
-
-import static br.com.eoxygen.enuns.TipoCampo.*;
-import static br.com.eoxygen.util.NomeColunaUtil.*;
-import static br.com.eoxygen.util.NomeTabelaUtil.*;
-
 import br.com.eoxygen.dto.AlocacaoRetornoDTO;
 import br.com.eoxygen.dto.AlocacaoUsinaDTO;
-import br.com.eoxygen.model.Usina;
 import br.com.eoxygen.model.UsinaSinai;
 import br.com.eoxygen.repository.GenericRepository;
 import br.com.eoxygen.util.InsertSQLUtil;
 import br.com.eoxygen.util.NomeTabelaUtil;
 import br.com.eoxygen.util.SQLUtil;
 import br.com.eoxygen.util.SelectSQLUtil;
-import org.checkerframework.checker.units.qual.A;
-
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import static br.com.eoxygen.enuns.TipoCampo.*;
+import static br.com.eoxygen.util.NomeColunaUtil.*;
+import static br.com.eoxygen.util.NomeTabelaUtil.*;
 
 public class UsinaService {
-    private String nomeSchema;
-    private GenericRepository repository;
-    private String nomeSchemaSinai = "sinai";
+
+    private String sqlGeral = "";
+
+    public final GenericRepository repository = new GenericRepository();
     private Long idCliente;
-
-
-    public UsinaService(){
-        this.repository = new GenericRepository();
-        this.nomeSchema = "cliente_";
-    }
-
-    public static void main(String[] args) throws Exception{
-        UsinaService usinaService = new UsinaService();
-        AlocacaoUsinaDTO dto = new AlocacaoUsinaDTO();
-        dto.setCnpjClienteNovo("88888888888");
-      //  dto.setCnpjClienteAntigo("2");
-        dto.setIdUsinaBaseSinai("1");
-        dto.setNovoNomeTopicoFirebase("firebase_usina_01");
-        dto.setNovoNomeTopicoIoT("topico_usina_01");
-        dto.setNovoNomeTabelaLeituraDynamon("leituras_usina_01");
-        usinaService.alocar(dto);
-
-
-    }
 
     public AlocacaoRetornoDTO alocar(AlocacaoUsinaDTO dto) throws Exception{
        String schemaClienteAntigo = "cliente_"+dto.getCnpjClienteAntigo();
-       String schemaClienteNovo = "cliente_"+dto.getCnpjClienteNovo();
-
        UsinaSinai usina = this.consultarUsinaBaseSinai(dto.getIdUsinaBaseSinai());
 
        if(!dto.getCnpjClienteAntigo().equals("")){
@@ -58,8 +31,8 @@ public class UsinaService {
 
        this.atualizarClienteUsinaBaseSinai(dto.getIdUsinaBaseSinai(),dto.getCnpjClienteNovo());
 
-       Long idAloacao = inserirAlocacaoUsinaCliente(usina.getId().toString());
-       Long idUsinaClienteNovo = this.inserirUsinaSchemaClienteNovo(usina,usina.getId().toString(),dto,idAloacao,dto.getNomeResponsavel(),dto.getContatoResponsavel(),dto.getIdHospitalBaseCliente());
+       this.inserirAlocacaoUsinaCliente(usina.getId().toString());
+       this.inserirUsinaSchemaClienteNovo(usina,usina.getId().toString(),dto,dto.getNomeResponsavel(),dto.getContatoResponsavel(),dto.getIdHospitalBaseCliente());
 
         AlocacaoRetornoDTO retorno = new AlocacaoRetornoDTO();
         retorno.setIdAlocacao(idAloacao);
@@ -72,10 +45,10 @@ public class UsinaService {
 
     private UsinaSinai consultarUsinaBaseSinai(String idUsina) throws Exception{
         String condicao = " AND "+TABELA_USINA_COLUNA_ID +" = "+ idUsina;
-        String sql = SQLUtil.select(nomeSchemaSinai,USINA,new SelectSQLUtil(),condicao);
+        String sql = SQLUtil.select(NOME_SCHEMA_BASE,USINA,new SelectSQLUtil(),condicao);
         ResultSet rs = this.repository.consultarDados(sql);
         UsinaSinai usina = null;
-        while(rs.next()) {
+        if(rs.next()) {
             usina = new UsinaSinai();
             usina.setId(rs.getLong(TABELA_USINA_SINAI_COLUNA_ID));
             usina.setNome(rs.getString(TABELA_USINA_SINAI_COLUNA_NOME));
@@ -85,18 +58,23 @@ public class UsinaService {
             usina.setDataCompra(rs.getDate(TABELA_USINA_SINAI_COLUNA_DATA_COMPRA));
             usina.setStatus(rs.getString(TABELA_USINA_SINAI_COLUNA_STATUS));
             usina.setIdCLP(rs.getString(TABELA_USINA_SINAI_COLUNA_ID_CLP));
+            rs.close();
+            return usina;
         }
-        return usina;
+        rs.close();
+        return null;
     }
 
     private void desativarUsinaSchemaClienteAntigo(String schemaClienteAntigo,String idUsinaSchemaBase) throws Exception{
         String sql = "UPDATE "+schemaClienteAntigo + "." + USINA + " SET " +TABELA_USINA_COLUNA_ATIVADA +
-                " = FALSE WHERE "+TABELA_USINA_COLUNA_ID_USINA_SCHEMA_SINAI + " = "+idUsinaSchemaBase;
-        System.out.println(sql);
-        this.repository.inseirDados(sql);
+                " = FALSE WHERE "+TABELA_USINA_COLUNA_ID_USINA_SCHEMA_SINAI + " = "+idUsinaSchemaBase +";";
+
+        this.sqlGeral = sqlGeral + sql;
     }
 
-    private Long inserirUsinaSchemaClienteNovo(UsinaSinai usina,String idUsinaSchemaBase, AlocacaoUsinaDTO dto,Long idAlocacao,String nomeResponsavel,String contatoResponsavel,String idHospital) throws Exception{
+    private Long inserirUsinaSchemaClienteNovo(UsinaSinai usina,String idUsinaSchemaBase, AlocacaoUsinaDTO dto,String nomeResponsavel,String contatoResponsavel,String idHospital) throws Exception{
+        String idAlocacao = "";
+
         String schemaClienteNovo = "cliente_"+dto.getCnpjClienteNovo();
         List<InsertSQLUtil> dados = new ArrayList<InsertSQLUtil>();
         dados.add(new InsertSQLUtil(TEXTO,TABELA_USINA_COLUNA_NOME,usina.getNome()));
@@ -116,45 +94,47 @@ public class UsinaService {
         dados.add(new InsertSQLUtil(TEXTO,TABELA_USINA_COLUNA_NOME_RESPONSAVEL,nomeResponsavel));
         dados.add(new InsertSQLUtil(TEXTO,TABELA_USINA_COLUNA_CONTATO_RESPONSAVEL,contatoResponsavel));
         dados.add(new InsertSQLUtil(NUMERO,TABELA_USINA_COLUNA_ID_HOSPITAL,idHospital));
-
-
-
         String sql = SQLUtil.insert(schemaClienteNovo,NomeTabelaUtil.USINA,dados);
-        System.out.println(sql);
+
         return  this.repository.inserirDadosComIdRetorno(sql);
     }
 
 
     private Long consultarIdClienteBaseSinai(String cnpjCliente) throws Exception{
         String condicao = " AND "+TABELA_CLIENTE_COLUNA_CNPJ +" = '"+ cnpjCliente + "'";
-        String sql = SQLUtil.select(nomeSchemaSinai,CLIENTE,new SelectSQLUtil(),condicao);
+        String sql = SQLUtil.select(NOME_SCHEMA_BASE,CLIENTE,new SelectSQLUtil(),condicao);
         ResultSet rs = this.repository.consultarDados(sql);
 
-        while(rs.next()) {
+        if(rs.next()) {
             idCliente = rs.getLong(TABELA_CLIENTE_COLUNA_ID);
         }
+
+        rs.close();
         return idCliente;
     }
 
     private void atualizarClienteUsinaBaseSinai(String idUsina,String cnpjCliente) throws Exception{
         String idCliente = this.consultarIdClienteBaseSinai(cnpjCliente).toString();
-        String sql = "UPDATE "+nomeSchemaSinai + "." + USINA + " SET " +TABELA_USINA_SINAI_COLUNA_ID_CLIENTE +
-                " = " +idCliente + " WHERE "+TABELA_USINA_SINAI_COLUNA_ID + " = "+idUsina;
-        System.out.println(sql);
-        this.repository.inseirDados(sql);
+        String sql = "UPDATE "+NOME_SCHEMA_BASE + "." + USINA + " SET " +TABELA_USINA_SINAI_COLUNA_ID_CLIENTE +
+                " = " +idCliente + " WHERE "+TABELA_USINA_SINAI_COLUNA_ID + " = "+idUsina + ";";
+        this.sqlGeral = sqlGeral + sql;
     }
 
 
-    private Long inserirAlocacaoUsinaCliente(String idUsina) throws Exception{
-        String schemaClienteNovo = "sinai";
+    private void inserirAlocacaoUsinaCliente(String idUsina) throws Exception{
         List<InsertSQLUtil> dados = new ArrayList<InsertSQLUtil>();
-        //dados.add(new InsertSQLUtil(DATA,TABELA_ALOCACAO_USINA_CLIENTE_DATA,usina.getNome()));
         dados.add(new InsertSQLUtil(NUMERO,TABELA_ALOCACAO_USINA_CLIENTE_ID_USINA,idUsina));
         dados.add(new InsertSQLUtil(NUMERO,TABELA_ALOCACAO_USINA_CLIENTE_ID_CLIENTE,idCliente.toString()));
 
-        String sql = SQLUtil.insert(schemaClienteNovo, ALOCACAO_USINA_CLIENTE,dados);
-        System.out.println(sql);
-        return this.repository.inserirDadosComIdRetorno(sql);
+        String sql = SQLUtil.insert(NOME_SCHEMA_BASE, ALOCACAO_USINA_CLIENTE,dados);
+
+        this.sqlGeral = sqlGeral + sql;
+
+        "SELECT ID FROM SINAI.ALOCACAO_USINA_CLIENTE "
+    }
+    private void consultarIdAlocacao(){
+        StringBuilder sql = n;
+
     }
 
 
